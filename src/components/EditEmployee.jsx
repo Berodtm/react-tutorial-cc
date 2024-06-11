95402706d9241215847a36e20dd626b5e2f872f4
Rob Dunn <dunnrob@hotmail.co.uk>
2024-06-11 21:03:43 +0100

    lesson 11 - added props as a value to the model options and writing the on change functions. next lesson passing values to the parent

--- src/components/EditEmployee.jsx
+++ src/components/EditEmployee.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function EditEmployee() {
+function EditEmployee(props) {
   const [show, setShow] = useState(false);
+  const [name, setName] = useState(props.name);
+  const [role, setRole] = useState(props.role);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -43,7 +45,10 @@ function EditEmployee() {
                   className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
                   id='name'
                   type='text'
-                  value='Jane Doe'
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
                 />
               </div>
             </div>
@@ -62,14 +67,16 @@ function EditEmployee() {
                   className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
                   id='role'
                   type='text'
-                  value='Jane Doe'
+                  defaultValue={role}
+                  onChange={(e) => {
+                    setRole(e.target.value);
+                  }}
                 />
               </div>
             </div>
           </form>
         </Modal.Body>
         <Modal.Footer>
-          
           <button
             className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
             form='editmodal'
